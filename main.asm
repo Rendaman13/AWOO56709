8755fd59fb38a5933886d91c87a9ed9e65ddd95e
Epsilon <renbannonpogothrowaway2@gmail.com>
2017-12-13 10:38:54 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -7,6 +7,21 @@ _oamlength EQU $A0
 _ram EQU $C000
 _ramlength EQU $2000
 _vram EQU $8000
+_xlocation EQUN _ram
+_ylocation EQU _ram + 1
+_srframecounter EQU _ram + 2
+_joypad EQU _ram + 3
+_justreleased _ram + 4
+_tilemap EQU _ram + 5 ; This is just where the tilemap starts
+_b EQU 1
+_a EQU 1 << 1
+_select EQU 1 << 2
+_start EQU 1 << 3
+_right EQU 1 << 4
+_left EQU 1 << 5
+_up EQU 1 << 6
+_down EQU 1 << 7 
+_srcombo EQU _select or _b or _start
 SECTION "Init",HOME[$150] ; Here we go
 Init:
 .wait
@@ -35,7 +50,7 @@ Init:
   halt ; Helps wth battery consumption
   nop ; Prevents a certain glith with halt
   call joypadpoll ; Polls joyinput
-  call srsetect ; Detects if SR inputs are being held down
+  call srsdetect ; Detects if SR inputs are being held down
   call cursorupdate ; Updates cursor accordingly
   jr gametick ; LOOOOOOOOP
  oamwipe:
@@ -46,8 +61,8 @@ Init:
   ldi [hl],a ; Load a into HL, increments HL
   dec c ; Decrement counter
   jr nz,.loop ; Counter isn't zero? LOOOOOP
-  ret ; We're done here
-  ramwipe:
+  ret ; We're done here 
+ramwipe:
     ld hl,_ram ; Loads RAM location into HL
     ld bc,_ramlength ; 16 bit counter
 .loop
@@ -58,7 +73,7 @@ Init:
     or c ; Check if both b and c are zero
     jr nz,.loop ; If they aren't,keep LOOOOPing
     ret ; Dunzo
-   vramwipe: ; Yeah basically the same as ramwipe except hL is VRAM
+vramwipe: ; Yeah basically the same as ramwipe except HL is VRAM
     ld hl,_vram ; Like I said, HL is vram
     ld bc,_ramlength ; 16 bit counter
 .loop
@@ -66,6 +81,60 @@ Init:
     ldi [hl],a ; Load a int HL, increment HL
     dec bc
     ld a,b
-    or c
+    or c 
     jr nz,.loop ; You're a mean one, Mr.LOOP
- ; Github is shaking the screen and it's triggering me
+    ret
+tilesetload: ; Lol I almost forgot this one
+  ld hl,Tileset ; Load tileset location into hl
+  ld de,_vram ; Vram is now in de
+  ld bc,16*2 ; each tile is 16 bytes long, and I only have two of them.
+.loop
+  ldi a,[hl] ; Nab current tile byte
+  ld [de],a ; Throw that shit into vram
+  inc de ; Increment de
+  dec bc ; Decrement counter
+  ld a,b
+  or c
+  jr nz,.loop
+  ret
+joypadpoll: MAI FAVORITE!!!
+  xor a ; a = 0
+  ld [_justreleased],a ; I only want justreleased to display for a frame or 2
+  ld a,%11011111 ; Set all of A's bits except 5, we're polling for button inputs
+  ld [rJOYP],a ; Tell the Joypad hardware register to wake the fuck up and poll for button inputs
+  rept 5
+  ld a,[rJOYP] ; Poll input, 5 times for accuracy
+  endr
+  cpl ; Need this because for some eason the GB's hardware resets bits if they are pressed instead of setting them. A set bit is much more convienent then a reset bit
+  and %1111 ; Nab the lower four bits for input
+  ld b,a ; Backup result, we'll need t later
+  ld a,%11101111 ; Now we reset bit four to poll for directional inputs
+  ld [rJOYP],a ; Sets the joypad hardware register to check for bit 4
+  rept 5
+  ld a,[rJOYP] ; Polls input, 5 times for accuracy
+  endr
+  cpl ; See above
+  and %1111 ; See above
+  swap a ; This is to make directional inputs distinguishable from button inputs
+  or b ; Add polled button inputs. Result is now in a.
+  ; Normally, we would be done here. But I wanted to take an extra step
+  ld hl,_joypad ; Takes whatever the joypad value was at last
+  cp [hl] ; Compares the current value with the current set joypad value
+  call nz,releasedetected ; Not the same? User released a button. Act accordingly/
+  ld [hl],a ; Put result into joypad address
+  ret ; Done
+releasedetected:
+  push af ; I need that later
+  ld a,[hl] ; Get last joypad input
+  ld [_justreleased],a ; Move it into justreleased
+  pop af ; I needed it
+  ret ; Bye
+srsdetect: ; With joypad inputs, we can now check if the user wants a soft reset. This is done with START+SELECT+B
+  ld a,[_joypad]
+  and _srcombo
+  ret ; Too lezzeh
+cursorupdate:
+  ret ; Too lezzeh
+mapupdate:
+  ret ; Too lezzeh
+  
