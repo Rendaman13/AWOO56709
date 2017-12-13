6bed1f1d4fac7aa255039cc8bd2b02601658ce8f
Epsilon <renbannonpogothrowaway2@gmail.com>
2017-12-13 10:44:55 -0500

    Replace HOME with ROM0

--- header.asm
+++ header.asm
@@ -1,21 +1,21 @@
 ; I fucking hate restart vectors. If the program hits one for some reason, make the game hang itself
-SECTION "Restart1",HOME[$00]
+SECTION "Restart1",ROM0[$00]
 rst $38
-SECTION "Restart2",HOME[$08]
+SECTION "Restart2",ROM0[$08]
 rst $38
-SECTION "Restart3",HOME[$10]
+SECTION "Restart3",ROM0[$10]
 rst $38
-SECTION "Restart4",HOME[$18]
+SECTION "Restart4",ROM0[$18]
 rst $38
-SECTION "Restart5",HOME[$20]
+SECTION "Restart5",ROM0[$20]
 rst $38
-SECTION "Restart6",HOME[$28]
+SECTION "Restart6",ROM0[$28]
 rst $38
-SECTION "Restart7",HOME[$30]
+SECTION "Restart7",ROM0[$30]
 rst $38
-SECTION "Restart8",HOME[$38]
+SECTION "Restart8",ROM0[$38]
 rst $38 ; lol
-SECTION "VblankJump",HOME[$40] ; Used for Vblank int
+SECTION "VblankJump",ROM0[$40] ; Used for Vblank int
 jp vblank ; Jump to Vblank handler
 SECTION "GameBoot",HOME[$100] ; Game jumps here on boot
 di ; ewwwwwwww interupts before init
@@ -23,7 +23,7 @@ jp start ; Jump to init
 rept $150 - $104
 db $00 ; RGBFIX likes header to be zero.
 endr
-SECTION "VblankHandler",HOME ; Handle VBLANK
+SECTION "VblankHandler",ROM0 ; Handle VBLANK
 push af
 push bc
 push de
