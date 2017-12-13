f786c3c0abf5d0ee3849591422e1da61f7de5ab2
Epsilon <renbannonpogothrowaway2@gmail.com>
2017-12-13 08:30:06 -0500

    Create tileset.asm

---
+++ tileset.asm
@@ -0,0 +1,6 @@
+SECTION "Tileset",HOME ; Since this ROM isn't that big, I can just kinda haphazardly throw this in the home bank
+Tileset: ; Literally one black tile and one white tile kappa
+DB $00,$00,$00,$00,$00,$00,$00,$00
+DB $00,$00,$00,$00,$00,$00,$00,$00
+DB $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+DB $FF,$FF,$FF,$FF,$FF,$FF,$FF,$FF
