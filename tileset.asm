a895826174b36376a4a9aaf19d188927f9dfa1a7
Epsilon <renbannonpogothrowaway2@gmail.com>
2017-12-13 10:46:25 -0500

    Replace HOME with ROM0

--- tileset.asm
+++ tileset.asm
@@ -1,4 +1,4 @@
-SECTION "Tileset",HOME ; Since this ROM isn't that big, I can just kinda haphazardly throw this in the home bank
+SECTION "Tileset",ROM0 ; Since this ROM isn't that big, I can just kinda haphazardly throw this in the home bank
 Tileset: ; Literally one black tile and one white tile kappa
 DB $00,$00,$00,$00,$00,$00,$00,$00
 DB $00,$00,$00,$00,$00,$00,$00,$00
